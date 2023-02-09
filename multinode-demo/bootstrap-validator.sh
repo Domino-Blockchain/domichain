bf2727e301219258fd419b1a02ee96c989257d34
rs-Nenad <jovanovski.n@redsteep.com>
2023-02-09 09:14:55 +0300

    add flag to show history

--- multinode-demo/bootstrap-validator.sh
+++ multinode-demo/bootstrap-validator.sh
@@ -143,6 +143,7 @@ args+=(
   --no-os-network-limits-test
   --no-wait-for-vote-to-start-leader
   --full-rpc-api
+  --enable-rpc-transaction-history
 )
 default_arg --gossip-port 8001
 default_arg --log -
