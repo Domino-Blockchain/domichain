6d3d29032e6b37368d88e93d776e0a133b7c933a
Ubuntu <ubuntu@ip-172-31-1-178.us-east-2.compute.internal>
2023-07-07 23:55:25 +0000

    tested on g4dn8x-02

--- aws-deploy/domichain_validator.sh
+++ aws-deploy/domichain_validator.sh
@@ -6,14 +6,21 @@ set -o errexit
 # Print executed commands
 set -o verbose
 
-#if [ -z "$1" ]
-#  then
-#    echo "No argument supplied: you must supply private IP address or bootstrap node"
-#    exit 1
-#fi
+if [ -z "$1" ]
+  then
+    echo "No argument supplied: you must supply private IP address or bootstrap node"
+    exit 1
+fi
+NODE_IP_ADDR="$1"
+export NODE_IP_ADDR
+URL=http://$NODE_IP_ADDR:8899
+export URL
+echo $NODE_IP_ADDR
+echo $URL
 
 cd ~/domichain
 rm -rf ~/domichain/config
+mkdir ~/domichain/config
 
 screen -d -m -S sys-tuner bash -c 'sudo $(command -v domichain-sys-tuner) --user $(whoami)'
 
@@ -36,7 +43,8 @@ if [ -n "$4" ]
 fi
 export WAIT_TIMEOUT
 
-./multinode-demo/setup.sh $slots_per_epoch
+#./multinode-demo/setup.sh $slots_per_epoch
+domichain-keygen new --force --no-passphrase -o ~/domichain/config/faucet.json
 FAUCET_PUBKEY=$(domichain-keygen pubkey ~/domichain/config/faucet.json)
 export FAUCET_PUBKEY
 domichain airdrop $airdrop_amount --url "$URL" "$FAUCET_PUBKEY"
@@ -48,7 +56,6 @@ screen -d -m -S validator bash -c "./multinode-demo/validator-x.sh \
   --gossip-port 8001 \
   --rpc-faucet-address $NODE_IP_ADDR:9900 \
   --allow-private-addr \
-  --vote-account ~/vote_account.json \
   > ~/stdout.txt 2> ~/stderr.txt"
 
 screen -d -m -S watch bash -c "watch \
@@ -63,32 +70,33 @@ export PUBKEY
 
 domichain airdrop 600 --url "$URL" "$PUBKEY"
 
-# function wait_for() {
-#     timeout=$WAIT_TIMEOUT
-#     shift 1
-#     until [ $timeout -le 0 ] || ("$@" &> /dev/null); do
-#         echo waiting for "$@"
-#         sleep 1
-#         timeout=$(( timeout - 1 ))
-#     done
-#     if [ $timeout -le 0 ]; then
-#         return 1
-#     fi
-# }
+ function wait_for() {
+     timeout=$WAIT_TIMEOUT
+     shift 1
+     until [ $timeout -le 0 ] || ("$@" &> /dev/null); do
+         echo waiting for "$@"
+         sleep 1
+         timeout=$(( timeout - 1 ))
+     done
+     if [ $timeout -le 0 ]; then
+         return 1
+     fi
+ }
 
-# function is_root_equal() {
-#   highest_root=$(domichain validators --url $URL --output json | jq '.validators | map(.rootSlot) | max')
+ function is_root_equal() {
+   highest_root=$(domichain validators --url $URL --output json | jq '.validators | map(.rootSlot) | max')
 
-#   vote_pubkey=$(domichain-keygen pubkey ~/domichain/config/validator-test1/vote-account.json)
-#   our_root=$(domichain validators --url $URL --output json | jq ".validators[] | select(.voteAccountPubkey == \"${vote_pubkey}\") | .rootSlot")
-#   if [ "${highest_root}" = "${our_root}" ]; then
-#     return 0;
-#   else
-#     return 1;
-#   fi
-# }
+   vote_pubkey=$(domichain-keygen pubkey ~/domichain/config/validator-test1/vote-account.json)
+   our_root=$(domichain validators --url $URL --output json | jq ".validators[] | select(.voteAccountPubkey == \"${vote_pubkey}\") | .rootSlot")
+   if [ "${highest_root}" = "${our_root}" ]; then
+     return 0;
+   else
+     return 1;
+   fi
+ }
 
 # echo "Waiting for sync slots with bootstrap validator"
-# wait_for 180 is_root_equal
+wait_for 180 is_root_equal
 
-./multinode-demo/delegate-stake.sh --vote-account ~/vote_account.json   --url "http://$NODE_IP_ADDR:8899/"   --label test1   --keypair ~/validator-stake-keypair.json   --force
+./multinode-demo/delegate-stake.sh  --label test1  --keypair ~/validator-stake-keypair.json  --url "http://$NODE_IP_ADDR:8899/" 50  --force
+echo "completed delegate-stake"
