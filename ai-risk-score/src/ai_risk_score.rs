d7c925eeecfabc0dfbf8c6a2599ce5ddb06663f7
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2023-09-25 11:37:42 -0400

    Update ai_risk_score.rs
    Added verification of signatures in retrieve risk scores response

--- ai-risk-score/src/ai_risk_score.rs
+++ ai-risk-score/src/ai_risk_score.rs
@@ -6,6 +6,8 @@ use std::sync::{Arc, RwLock};
 use serde::Deserialize;
 use std::time::Duration;
 use tokio::time;
+use ed25519_dalek::{PublicKey, Signature};
+use hex::FromHex;
 
 lazy_static! {
     pub static ref RISK_SCORE_MAP: Arc<RwLock<HashMap<String, HashMap<String, f64>>>> =
@@ -23,6 +25,17 @@ struct ParsedResponse {
     data: String,
     public_key: String,
     signature:String,
+}
+
+fn verify_signature(data_hex: &str, signature_hex: &str, public_key_hex: &str) -> bool {
+    let data_bytes = Vec::from_hex(data_hex).expect("Failed to decode data hex string");
+    let signature_bytes = Vec::from_hex(signature_hex).expect("Failed to decode signature hex string");
+    let public_key_bytes = Vec::from_hex(public_key_hex).expect("Failed to decode public key hex string");
+
+    let public_key = PublicKey::from_bytes(&public_key_bytes).expect("Failed to create PublicKey from bytes");
+    let signature = Signature::from_bytes(&signature_bytes).expect("Failed to create Signature from bytes");
+
+    public_key.verify(&data_bytes, &signature).is_ok()
 }
 
 pub async fn get_risk_score(url:String, ai_reward_rate:f64) {
@@ -51,21 +64,26 @@ pub async fn get_risk_score(url:String, ai_reward_rate:f64) {
 
         info!("---AI test parsed_response {:?}", parsed_response);
         {
-        let mut risk_score_map = RISK_SCORE_MAP.write().unwrap();
-        for entry in parsed_response {
-            let wallet = entry.wallet;
-            let reward_account: String = entry.public_key;
-            let risk_score = entry.risk_score;
-            //println!("Wallet: {:?}, Risk Score: {:?}", wallet, risk_score);
-            let wallet_entry = risk_score_map.entry(wallet).or_insert_with(HashMap::new);
-            let rewards_entry = wallet_entry.entry(reward_account);
-            rewards_entry.or_insert(risk_score);
+            let mut risk_score_map = RISK_SCORE_MAP.write().unwrap();
+            for entry in &parsed_response {
+                let wallet = &entry.wallet;
+                let reward_account: &String = &entry.public_key;
+                let risk_score = entry.risk_score;
+
+                let data_hex = &entry.data;
+                let signature_hex = &entry.signature;
+                let public_key_hex = &entry.public_key;
+                let is_signature_valid = verify_signature(data_hex, signature_hex, public_key_hex);
+
+                let wallet_entry = risk_score_map.entry(wallet.to_owned()).or_insert_with(HashMap::new);
+                let rewards_entry = wallet_entry.entry(reward_account.to_owned());
+                rewards_entry.or_insert(risk_score);
+            }
+            
+            drop(risk_score_map);
         }
-        
-        drop(risk_score_map);
-    }
+
         time::sleep(Duration::from_secs(QUERY_TIME_PERIOD)).await;
-        
     }
 }
 
