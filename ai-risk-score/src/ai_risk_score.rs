575b2d476128d75e30890445f15583f5e3471689
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2023-09-25 15:40:19 -0400

    Update ai_risk_score.rs
    Added signature verification check

--- ai-risk-score/src/ai_risk_score.rs
+++ ai-risk-score/src/ai_risk_score.rs
@@ -1,13 +1,14 @@
-use lazy_static::lazy_static;
-use log::{info, warn};
-use tokio::sync::OnceCell;
-use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
-use serde::Deserialize;
-use std::time::Duration;
-use tokio::time;
+use bs58;
 use ed25519_dalek::{PublicKey, Signature};
 use hex::FromHex;
+use lazy_static::lazy_static;
+use log::{info, warn};
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use std::time::Duration;
+use tokio::sync::OnceCell;
+use tokio::time;
 
 lazy_static! {
     pub static ref RISK_SCORE_MAP: Arc<RwLock<HashMap<String, HashMap<String, f64>>>> =
@@ -16,7 +17,7 @@ lazy_static! {
 
 pub static AI_REWARDS_RATE: OnceCell<f64> = OnceCell::const_new();
 
-const QUERY_TIME_PERIOD:u64 = 600;
+const QUERY_TIME_PERIOD: u64 = 600;
 
 #[derive(Debug, Deserialize)]
 struct ParsedResponse {
@@ -24,23 +25,33 @@ struct ParsedResponse {
     wallet: String,
     data: String,
     public_key: String,
-    signature:String,
+    signature: String,
+}
+
+fn from_base58_str(s: &str) -> Vec<u8> {
+    bs58::decode(s)
+        .into_vec()
+        .expect("Failed to decode base58 string")
 }
 
 fn verify_signature(data_hex: &str, signature_hex: &str, public_key_hex: &str) -> bool {
     let data_bytes = Vec::from_hex(data_hex).expect("Failed to decode data hex string");
-    let signature_bytes = Vec::from_hex(signature_hex).expect("Failed to decode signature hex string");
-    let public_key_bytes = Vec::from_hex(public_key_hex).expect("Failed to decode public key hex string");
+    let signature_bytes =
+        Vec::from_hex(signature_hex).expect("Failed to decode signature hex string");
+    let public_key_bytes = from_base58_str(public_key_hex);
 
-    let public_key = PublicKey::from_bytes(&public_key_bytes).expect("Failed to create PublicKey from bytes");
-    let signature = Signature::from_bytes(&signature_bytes).expect("Failed to create Signature from bytes");
+    let public_key =
+        PublicKey::from_bytes(&public_key_bytes).expect("Failed to create PublicKey from bytes");
+    let signature =
+        Signature::from_bytes(&signature_bytes).expect("Failed to create Signature from bytes");
 
     public_key.verify(&data_bytes, &signature).is_ok()
 }
 
-pub async fn get_risk_score(url:String, ai_reward_rate:f64) {
-
-    AI_REWARDS_RATE.set(ai_reward_rate).expect("Failed to set AI rewards rate");
+pub async fn get_risk_score(url: String, ai_reward_rate: f64) {
+    AI_REWARDS_RATE
+        .set(ai_reward_rate)
+        .expect("Failed to set AI rewards rate");
     loop {
         // Use the tokio runtime to run the asynchronous function and get the JSON response.
         let json_response = match send_http_request(&url).await {
@@ -52,7 +63,7 @@ pub async fn get_risk_score(url:String, ai_reward_rate:f64) {
             }
         };
         info!("Response from AI node {:?}", json_response);
-        
+
         let parsed_response: Vec<ParsedResponse> = match serde_json::from_str(&json_response) {
             Ok(parsed) => parsed,
             Err(err) => {
@@ -75,19 +86,23 @@ pub async fn get_risk_score(url:String, ai_reward_rate:f64) {
                 let public_key_hex = &entry.public_key;
                 let is_signature_valid = verify_signature(data_hex, signature_hex, public_key_hex);
 
-                let wallet_entry = risk_score_map.entry(wallet.to_owned()).or_insert_with(HashMap::new);
-                let rewards_entry = wallet_entry.entry(reward_account.to_owned());
-                rewards_entry.or_insert(risk_score);
+                if is_signature_valid {
+                    let wallet_entry = risk_score_map
+                        .entry(wallet.to_owned())
+                        .or_insert_with(HashMap::new);
+                    let rewards_entry = wallet_entry.entry(reward_account.to_owned());
+                    rewards_entry.or_insert(risk_score);
+                } else {
+                    warn!("Invalid signature for wallet: {}", wallet);
+                }
             }
-            
+
             drop(risk_score_map);
         }
 
         time::sleep(Duration::from_secs(QUERY_TIME_PERIOD)).await;
     }
 }
-
-
 
 async fn send_http_request(url: &str) -> Result<String, reqwest::Error> {
     let client = reqwest::Client::new();
