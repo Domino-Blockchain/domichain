543dbbc995928191876fca7a546ebd447bdc0490
shujun ding <info@dominochain.com>
2023-11-22 22:13:10 -0500

    temp fix for slow confirmation

--- core/src/cluster_info_vote_listener.rs
+++ core/src/cluster_info_vote_listener.rs
@@ -1,3 +1,5 @@
+use domichain_sdk::native_token::SATOMIS_PER_DOMI;
+
 use {
     crate::{
         banking_trace::{BankingPacketBatch, BankingPacketSender},
@@ -401,8 +403,8 @@ impl ClusterInfoVoteListener {
                         ]);
 
                         let weight = sortition::select(
-                            stake,
-                            total_stake,                // Maybe use circulation across net?
+                            stake/1000,
+                            total_stake/1000,                // Maybe use circulation across net?
                             total_weight as f64,  // Consensus params
                             h,
                         );
@@ -764,8 +766,8 @@ impl ClusterInfoVoteListener {
                         ]);
 
                         let weight = sortition::select(
-                            stake,
-                            total_stake,
+                            stake/1000,
+                            total_stake/1000,
                             total_weight as f64,
                             h,
                         );
